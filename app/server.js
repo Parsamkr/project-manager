a73d9399c24fa2a2131f356a1374407d0ed8443d
Mohammad Parsa Mokhtarpour <parsa.mokhtarpour@yahoo.com>
2023-08-14 09:40:52 +0330

    create server and config application

---
+++ app/server.js
@@ -0,0 +1,50 @@
+module.exports = class Application {
+  #express = require("express");
+  #app = this.#express();
+
+  constructor(PORT, DB_URL) {
+    this.configDatabase(DB_URL);
+    this.configApplication();
+    this.createServer(PORT);
+    this.createRoutes();
+    this.errorHandler();
+  }
+  configApplication() {
+    const path = require("path");
+    this.#app.use(this.#express.json());
+    this.#app.use(this.#express.urlencoded({ extended: true }));
+    this.#app.use(this.#express.static(path.join(__dirname, "..", "public")));
+  }
+  createServer(PORT) {
+    const http = require("http");
+    const server = http.createServer(this.#app);
+    server.listen(PORT, () => {
+      console.log(`server run on port : http://localhost:${PORT}`);
+    });
+  }
+  configDatabase(DB_URL) {
+    const mongoose = require("mongoose");
+    mongoose.connect(DB_URL, (error) => {
+      if (error) throw error;
+      return console.log("Successfully Connected to DB ...");
+    });
+  }
+  errorHandler() {
+    this.#app.use((req, res, next) => {
+      return res
+        .status(404)
+        .json({ status: 404, success: false, message: "Route doesn't exist" });
+    });
+
+    this.#app.use((error, req, res, next) => {
+      const status = error?.status || 500;
+      const message = error?.message || "InternalServerError";
+      return res.status(status).json({ status, success: false, message });
+    });
+  }
+  createRoutes() {
+    this.#app.get("/", (req, res, next) => {
+      return res.json({ message: "this a new application" });
+    });
+  }
+};
